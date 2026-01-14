70a7e9d9e9d386e0e0fc8519aeffda7bfa5e24eb
rikdehoop <71008894+rikdehoop@users.noreply.github.com>
2026-01-14 14:39:19 +0100

    Update collect_osmdata.py

--- collect_osmdata.py
+++ collect_osmdata.py
@@ -1,11 +1,13 @@
 import osmnx as ox
 import geopandas as gpd
 import matplotlib.pyplot as plt
-# Specify the name that is used to seach for the data
-# List key-value pairs for tags
-tags = {"shop": "supermarket"}   
+import os
 from shapely.geometry import Polygon
 
+GLOBAL_CWD = os.getcwd()
+
+tag1 = {"shop": "supermarket"} 
+tag2 = {"amenity": "doctors"}  
 polygon = Polygon([
     (5.25, 51.65),
     (5.36, 51.65),
@@ -13,15 +15,38 @@ polygon = Polygon([
     (5.25, 51.75),
     (5.25, 51.65)
 ])
-supermarkten = ox.features_from_polygon(polygon, tags)
-supermarkten.head()
-# Plot footprints 
-supermarkten.plot()
-plt.show()
+def getrequest_osm(**kwargs):
+    path = os.path.join(GLOBAL_CWD, "store_all_data")
+    supermarkten = ox.features_from_polygon(kwargs["polygon"], kwargs["tag1"])
+    artsen = ox.features_from_polygon(kwargs["polygon"], kwargs["tag2"])
+    supermarkten.head()
+    artsen.head()
+    # Plot footprints 
+    supermarkten.plot()
+    artsen.plot()
+    plt.show()
 
-supermarkten  = supermarkten.loc[:,supermarkten.columns.str.contains('addr:|geometry')]
-supermarkten = supermarkten.loc[supermarkten.geometry.type=='Point']
+    supermarkten  = supermarkten.loc[:,supermarkten.columns.str.contains('addr:|geometry')]
+    supermarkten = supermarkten.loc[supermarkten.geometry.type=='Point']
+    artsen  = artsen.loc[:,artsen.columns.str.contains('addr:|geometry')]
+    artsen = artsen.loc[artsen.geometry.type=='Point']
+    supermarkten.to_file(f"{path}\\{kwargs["output_fn1"]}", layer="lyr1", driver="GPKG")  
+    artsen.to_file(f"{path}\\{kwargs["output_fn2"]}", layer="lyr2", driver="GPKG")  
 
-supermarkten.to_file("C:\\Users\\RikdeHoop\\Documents\\Local_proj\\Personal_Python_Tools\\store_all_data\\supermarkten.gpkg", layer="supermarkten", driver="GPKG")  
+    
+getrequest_osm(**dict(tag1 = {"shop": "supermarket"},
+                      tag2 = {"amenity": "doctors"},
+                      output_fn1 = "supermarkten.gpkg",
+                      output_fn2 = "artsen.gpkg",
+                      polygon = Polygon([
+                          (5.25, 51.65),
+                          (5.36, 51.65),
+                          (5.36, 51.75),
+                          (5.25, 51.75),
+                          (5.25, 51.65)
+                          ])
+                          ))
+
+
 # Or save in a more open source format
 #supermarkten.to_file('../temp/edgewood_supermarkten.geojson', driver='GeoJSON')  
